fe3c67dfc8a79245d54d626225b2ccae05c19260
Micks-Tech <mickstech060924@gmail.com>
2026-01-28 16:26:35 +0800

    commit changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About() {
           <h3 className='text-center mt-6 font-bold text-3xl uppercase text-slate-700 font-sans'>About</h3>
           <div className='flex justify-center items-center h-full w-1/2 gap-4'>
               <img src={ImageAbout} alt=""
-              className='w-1/2 h-1/2 drop-shadow-2xl size-16 animate-bounce'/>
+              className='w-1/2 h-1/2 drop-shadow-2xl'/>
               <p className='w-full p-2 text-justify text-slate-700'>
                   I am a fresh graduate of Lemery Colleges, where I earned a Bachelor of Science in
                   Information Technology. I have a strong interest in software development and
