e0880b7929a3fc745b6ba782a7bfa29d663285cf
MicksTech <accpersonal371@gmail.com>
2026-02-08 23:12:43 +0800

    Remove social media links from Footer component
    Removed social media links from the footer component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,29 +37,6 @@ export default function Footer() {
                         <FontAwesomeIcon icon={faAirbnb} />
                     </a>
                 </div>
-                <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-5  gap-4 my-4'>
-                    <a href="https://www.facebook.com/johnmichael.b.castor.58" target='_blank' className='px-4 py-2 gap-2 flex bg-slate-500 hover:bg-slate-600 text-white rounded'>
-                        Facebook
-                        <FontAwesomeIcon icon={faFacebook} />
-                    </a>
-                    <a href="https://www.youtube.com/@m-castorjohnmichaelbalasbs3039" target='_blank' className='px-4 py-2 gap-2 flex bg-slate-300 hover:bg-slate-400 rounded text-slate-700'>
-                        Youtube
-                        <FontAwesomeIcon icon={faYoutube} />
-                    </a>
-                    <a href="https://www.instagram.com/mickstech7/" target='_blank' className='px-4 py-2 gap-2 flex bg-slate-300 hover:bg-slate-400 rounded text-slate-700'>
-                        Instagram
-                        <FontAwesomeIcon icon={faInstagram} />
-                    </a>
-                    <a href="https://github.com/MicksTech" target='_blank' className='px-4 py-2 gap-2 flex bg-slate-300 hover:bg-slate-400 rounded text-slate-700'>
-                        Github
-                        <FontAwesomeIcon icon={faGithub} />
-                    </a>
-
-                    <a href="" className='px-4 py-2 gap-2 flex bg-slate-500 hover:bg-slate-600 text-white rounded'>
-                        Send Email
-                        <FontAwesomeIcon icon={faAirbnb} />
-                    </a>
-                </div>
                 <span className='my-4 text-slate-800 dark:text-white'>
                     <FontAwesomeIcon icon={faCopyright} />
                     {currentDate} Michael. All rights reserved
